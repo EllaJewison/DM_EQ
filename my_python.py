7353a5af44491156d8c396b7cd8ec25ffd9d7364
Sassaraf <annaelassaraf@gmail.com>
2022-11-10 14:09:20 +0200

    update last minut clean up

--- my_python.py
+++ my_python.py
@@ -1,6 +1,7 @@
 """This is the project of Ella, Emuna and Salomé
 on the earthquake website
 "https://www.allquakes.com/earthquakes/today.html" """
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,7 +9,6 @@ from bs4 import BeautifulSoup
 def create_soup_from_link(link):
     """ creates soup from link """
     page = requests.get(link)  # asking permission from the website to fetch data, If response is 200 its ok
-    # print(page.text)
     my_soup = BeautifulSoup(page.content, "html.parser")  # creating a bs object that takes page.content as an input
     return my_soup
 
@@ -44,6 +44,7 @@ def cleans_from_html_nonsense(table) -> list:
 
     headers = cells[0:6]
     table = []
+    print("rows of clean table")
     for i in range(7, len(cells), len(headers)):
         row = cells[i:i + 6]
         print(row)
@@ -52,34 +53,22 @@ def cleans_from_html_nonsense(table) -> list:
     return table
 
 
-# URL = "https://www.allquakes.com/earthquakes/today.html"
-# soup = create_soup_from_link(URL)
-# page = requests.get(URL)  # asking permission from the website to fetch data, If response is 200 its ok
-# # print(page.text)
-#
-# soup = BeautifulSoup(page.content, "html.parser")  # creating a bs object that takes page.content as an input
-
-
-# (page.content is the HTML content I just scraped) sometime you need to change the second arg to another appropriate
-
-# # this gets all the html dat a from the "visible" table
-# table_EQ = soup.find('table', id="qTable")
-
 def get_eq(table):
     """this finds all the earthquakes from magnitude 2 to magnitude 5"""
     return table.find_all(class_=lambda text: text in ['q2', 'q3', 'q4', 'q5'])
 
 
-def find_quake_id(table) -> list:
-    """ function that creates a list of quake ID. We will need it for getting the info on the second page
-    Used for function in second page
-    """
-    quakes = get_eq(table)
-    quake_id_list = []
-    for q in quakes:
-        eq_id = q.get('id')
-        quake_id_list.append(eq_id)
-    return quake_id_list
+# we will need the function later on
+# def find_quake_id(table) -> list:
+#     """ function that creates a list of quake ID. We will need it for getting the info on the second page
+#     Used for function in second page
+#     """
+#     quakes = get_eq(table)
+#     quake_id_list = []
+#     for q in quakes:
+#         eq_id = q.get('id')
+#         quake_id_list.append(eq_id)
+#     return quake_id_list
 
 
 def find_url_in_table_eq(table) -> list:
@@ -92,17 +81,18 @@ def find_url_in_table_eq(table) -> list:
         info_link = (q.find_all("a"))[-1]
         link_url = info_link["href"]
         url_list.append(link_url)
-    print(url_list)
+    # print(url_list)
     return url_list
 
 
-def scrap_from_p2(quake_id, quake_url):
+def scrap_from_p2(quake_url) -> object:
     """ This function takes the quake id and the url link of an earthquake,  and opens the "more page" and
     hopefully retrieves the data  """
     url = "https://www.volcanodiscovery.com/" + quake_url
     my_soup = create_soup_from_link(url)
     table_p2 = []
     table_rows = my_soup.find_all('tr')
+    print("print inner page more tables by row")
     for tr in table_rows:
         td = tr.find_all('td')
         row = [i.text for i in td]
@@ -118,26 +108,24 @@ def main_scrapper_p1():
     # table_eq_dirty = builds_table_eq()
     table_eq_dirty = builds_table_show_more(soup)
     table_eq = cleans_from_html_nonsense(table_eq_dirty)
-    print(table_eq)
+    # print('the main table data:\n', table_eq)
 
     url_list = find_url_in_table_eq(table_eq_dirty)
-    quake_id_list = find_quake_id(table_eq_dirty)
+    # quake_id_list = find_quake_id(table_eq_dirty)#we will
 
     # this scrapes the second page !!!!!!!!!!
     counter = 1
-    tuple_id = zip(quake_id_list, url_list)
-    for elem in tuple_id:
+    # tuple_id = zip(quake_id_list, url_list)
+    for elem in url_list:
         # print(elem)
-        table_p2 = scrap_from_p2(elem[0], elem[1])
-
+        table_p2 = scrap_from_p2(elem)
         print('Table num: ', counter)
         counter += 1
-        print('table_p2 : ', table_p2)
-        print()
+        # print('table_p2 : ', table_p2)
 
 
 def main():
-    print(main_scrapper_p1())
+    main_scrapper_p1()
 
 
 if __name__ == '__main__':
