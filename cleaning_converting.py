a8336dd8a9fbf27cd3409fa0a5a9c212f6c1d1df
Emuna Lipsker <emunac@gmail.com>
2022-11-28 22:26:31 +0200

    chnages from emuna - near by towns

--- cleaning_converting.py
+++ cleaning_converting.py
@@ -4,6 +4,7 @@ import numpy as np
 import re
 import pandas as pd
 
+
 def set_epicenter_coord(str_epicenter):
     pattern = r"([\d\.]+)°([SN])[^\d]+([\d\.]+)°([EW])"
     result = re.search(pattern, str_epicenter)
@@ -21,74 +22,88 @@ def set_epicenter_coord(str_epicenter):
     # there is also the information on the country and region, do we need this ? its stored in grp5 if needed.
     return coord
 
+
 def energy_release(e):
     result = re.search(r"([\d.]+) x 10(\d+)", e)
     mantis = float(result[1])
     exponent = int(result[2])
     return mantis * np.power(10, exponent)
-#============================================================================
 
 
-def test_set_datatype_status(df):
-    d = df.copy()
-    set_datatype_status(d)
-    print(df['Status'])
-    print(d['Status'])
+def extract_cities_info(city_string):
+    if city_string is np.nan:
+        return np.nan
+    city_string = re.sub(r'^.*}}', '', city_string)
+    cities = city_string.split('| Show on map | Quakes nearby')
+    matches = [re.match(r'(^\d+) .*\) (.*) \(pop: (\d+),(\d+)', city) for city in cities]
+    info = [(int(m.group(1)), m.group(2), 1000 * int(m.group(3)) + int(m.group(4))) for m in matches if m]
+    return info
 
-if __name__ == '__main__':
-    df = pd.read_csv('earthquake.csv')
 
+# ============================================================================
+
+def main(df):
     columns_to_drop = ["Local time at epicenter"]
     df.drop(columns_to_drop, axis=1)
 
     # Status
     df['Status'] = df['Status'] == "Confirmed"
 
-    #Date & time
-    df['Date & time'] = df['Date & time'].apply(lambda x: datetime.strptime(re.sub('UTC.*', 'UTC', x),'%b %d, %Y %H:%M:%S UTC'))
+    # Date & time
+    df['Date & time'] = df['Date & time'].apply(
+        lambda x: datetime.strptime(re.sub('UTC.*', 'UTC', x), '%b %d, %Y %H:%M:%S UTC'))
 
-    #Magnitude
+    # Magnitude
     df['Magnitude'] = df['Magnitude'].where(~df['Magnitude'].str.startswith("unknown"), np.nan)
-    df['Magnitude']= df['Magnitude'].astype(float)
-
+    df['Magnitude'] = df['Magnitude'].astype(float)
 
     # Depth
-    df['Depth']= df['Depth'].str.replace(" km", '')  # just to get rid of the km str at the end
-    df['Depth']= df['Depth'].astype(float)
+    df['Depth'] = df['Depth'].str.replace(" km", '')  # just to get rid of the km str at the end
+    df['Depth'] = df['Depth'].astype(float)
 
-    #Epicenter latitude / longitude
+    # Epicenter latitude / longitude
     df["Epicenter latitude / longitude"] = df["Epicenter latitude / longitude"].apply(set_epicenter_coord)
 
-    #Antipode
+    # Antipode
     df["Antipode"] = df["Antipode"].apply(set_epicenter_coord)
 
     # Shaking intensity
-    intensity_str_to_nbr = {'Not felt':0,
-                            'Very weak shaking':1,
-                            'Weak shaking near epicenter':1,
-                            'Light shaking near epicenter':1,
-                            'Light shaking':1,
-                            'Weak shaking':2,
-                            'Moderate shaking near epicenter':3,
-                            'Strong shaking near epicenter':4}
+    intensity_str_to_nbr = {'Not felt': 0,
+                            'Very weak shaking': 1,
+                            'Weak shaking near epicenter': 1,
+                            'Light shaking near epicenter': 1,
+                            'Light shaking': 1,
+                            'Weak shaking': 2,
+                            'Moderate shaking near epicenter': 3,
+                            'Strong shaking near epicenter': 4}
     df["Shaking intensity"] = df["Shaking intensity"].apply(lambda x: intensity_str_to_nbr[x])
 
     # Felt
     df["Felt"] = df["Felt"].where(df["Felt"].notnull(), "0")
     df["Felt"] = df["Felt"].apply(lambda x: int(re.search(r'^\d*', x)[0]))
 
-    #Estimated seismic energy released
-    df["Estimated seismic energy released"] = df["Estimated seismic energy released"].where(df["Estimated seismic energy released"].notnull(), np.nan)
-    df["Estimated seismic energy released"][df["Estimated seismic energy released"].notnull()] = df["Estimated seismic energy released"][df["Estimated seismic energy released"].notnull()].apply(energy_release)
+    # Estimated seismic energy released
+    df["Estimated seismic energy released"] = df["Estimated seismic energy released"].where(
+        df["Estimated seismic energy released"].notnull(), np.nan)
+    df["Estimated seismic energy released"][df["Estimated seismic energy released"].notnull()] = \
+        df["Estimated seismic energy released"][df["Estimated seismic energy released"].notnull()].apply(energy_release)
+    df['Nearby towns and cities'] = df['Nearby towns and cities'].apply(extract_cities_info)
+    return df
+
+if __name__ == '__main__':
+    df = pd.read_csv('/home/emuna/Documents/Itc/DM_EQ/earthquake.csv')
+    df = main(df)
+
 
 
     # tests
-    # print(df['Date & time'])
-    # print(df['Magnitude'])
-    # print(df['Depth'])
-    # print(df["Epicenter latitude / longitude"])
-    # print(df["Antipode"])
-    # print(df["Shaking intensity"])
-    # print(df["Felt"])
-    print(df["Estimated seismic energy released"])
 
+    print(df['Date & time'])
+    print(df['Magnitude'])
+    print(df['Depth'])
+    print(df["Epicenter latitude / longitude"])
+    print(df["Antipode"])
+    print(df["Shaking intensity"])
+    print(df["Felt"])
+    print(df["Estimated seismic energy released"])
+    print(df["Nearby towns and cities"])
